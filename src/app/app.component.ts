0821567bb2a121feacda7d29d7c740b5c3dcd149
Jeff <jm@jeffz.com>
2021-07-28 18:56:54 -0400

    Empty project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,27 +4,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //template: '<h1>This is from the component</h1>',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'project2';
-  mynumber: number = 123;
-
-  constructor() {
-    console.log('In the constructor');
-  }
-
-  ngOnChanges() {
-    console.log('In the ngOnChanges handler');
-  }
-
-  ngOnInit() {
-    console.log(
-      'In the ngOnInit handler with title=',
-      this.title,
-      '  mynumber=',
-      this.mynumber
-    );
-  }
-}
+export class AppComponent {}
