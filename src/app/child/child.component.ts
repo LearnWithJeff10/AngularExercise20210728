42822d33fbe0e26580cd2eb5630866c8e18d8d40
Jeff <jm@jeffz.com>
2021-07-28 19:40:15 -0400

    Empty project again

--- src/app/child/child.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css'],
-})
-export class ChildComponent {}
