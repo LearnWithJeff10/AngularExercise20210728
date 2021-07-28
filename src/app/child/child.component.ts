e2d3833add4e8e556bc602d9f9e859d4f6c9c0d9
Jeff <jm@jeffz.com>
2021-07-28 19:15:32 -0400

    Added child component

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css'],
+})
+export class ChildComponent {}
