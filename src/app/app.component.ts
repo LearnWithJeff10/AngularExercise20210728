83ccbaad325a3969772bedf0f761417c51f3e222
Jeff <jm@jeffz.com>
2021-07-28 18:44:39 -0400

    Style demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //template: '<h1>This is from the component</h1>',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'project2';
