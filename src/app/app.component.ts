3d2a12ef2316816cf5b56e6e37750199cb4dbb24
Jeff <jm@jeffz.com>
2021-07-28 20:13:35 -0400

    Almost empty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  myNumber = 123;
-  myArray = [11, 22, 33];
-  myDate = new Date();
-  myBool = true;
-  myString = 'Hi guys and girls';
-
-  myMethod() {
-    return 'This is a string coming from a method';
-  }
-
-  myAlert() {
-    alert('In myAlert');
-    console.log('In myAlert testing...');
-  }
-}
+export class AppComponent {}
